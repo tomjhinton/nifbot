ef434bb74fdffa9104d3a7e0be5df93c7ad6f647
Tom Hinton <tomjhinton@gmail.com>
2020-02-01 20:55:44 +0000

    test

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ var client = new Twitter({
 
 let ans =
 
-axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.giphy_key}&q=next%20in%20fashion`)
+axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.giphy_key}&q=next%20in%20fashion&limit=100`)
   .then(response => {
-    console.log(response.data.data[0].images['fixed_height'].url)
-    ans =response.data.data.filter(x=> x.slug.includes('netflix-fashion-designer'))[Math.floor(Math.random()*response.data.data.length)].bitly_gif_url
+    // console.log(response.data.data[0].images['fixed_height'].url)
+    ans =response.data.data.filter(x=> x.slug.includes('netflix-fashion-designer'))[Math.floor(Math.random()*response.data.data.length)-1].bitly_gif_url
     console.log(ans.length)
     client.post('statuses/update', {status: ans},  function(error, tweet, response) {
   if(error) throw error;
